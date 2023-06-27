3f7d6ca72a37787909cc6c43ebceb2d88a836dbd
Aldair2209 <andradegutierrezaldairsmith@gmail.com>
2023-06-27 17:20:28 -0500

    actualizacion

--- archivos/js/main.js
+++ archivos/js/main.js
@@ -1,48 +1,184 @@
 
 /*=============== HOME ===============*/
 
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+const sr = ScrollReveal({
+  distance: '65px',
+  duration: 2600,
+  delay: 100,
+  reset: true
 });
 
-sr.reveal('.hero-text', {delay:200, origin:'top'});
-sr.reveal('.hero-img', {delay:450, origin:'top'});
-sr.reveal('.icons', {delay:500, origin:'left'});
+sr.reveal('.hero-text, .section__title', { delay: 400, origin: 'top' });
+sr.reveal('.hero-img', { delay: 500, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left', interval: 200 });
+sr.reveal('.form1', { delay: 500, origin: 'right' });
+sr.reveal('.card_noticias', { delay: 500, origin: 'bottom', interval: 300 });
 
+/*=============== NUESTROS DOCENTES ===============*/
 
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 5000);
+}
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+//section docente
+var cardTitles = document.querySelectorAll('.carousel .card h2, .carousel .card span');
+var options = {
+  rootMargin: '0px',
+  threshold: 0.2 // Porcentaje del elemento visible necesario para activar el cambio
+};
+
+var callback = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      // El elemento está en la vista
+      entry.target.style.animation = 'typing 0.8s ease-in-out forwards';
+    } else {
+      // El elemento está fuera de la vista
+      entry.target.style.animation = 'none';
+    }
+  });
+};
+
+var observer = new IntersectionObserver(callback, options);
+
+cardTitles.forEach(function (title) {
+  observer.observe(title);
+});
 
 
 /*=============== SECCIONES DE DESPLAZAMIENTO ACTIVE LINK ===============*/
 
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
 
 
 /*=============== CAMBIAR EL FONDO HEADER ===============*/
 
-function scrollHeader(){
-    const header = document.getElementById('header')
-    
-// Cuando el desplazamiento es mayor que 80 de altura de ventana gráfica, agregue la clase de encabezado de desplazamiento a la etiqueta del encabezado
-    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+  const header = document.getElementById('header')
+
+  // Cuando el desplazamiento es mayor que 80 de altura de ventana gráfica, agregue la clase de encabezado de desplazamiento a la etiqueta del encabezado
+  if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+
+/*=============== js del contactanos ===============*/
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
